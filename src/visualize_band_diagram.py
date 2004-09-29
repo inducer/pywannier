edd25b1a37711cb0a73b13f7f8033f5d6d05e97b
Andreas Kloeckner <ak@ixion.net>
2004-09-29 02:18:01 +0000

    [pywannier @ Arch-1:ak@ixion.net--2003-diplomarbeit%pywannier--prototype--1.0--patch-46] visualizing 1D band diagrams works, except for a spurious factor of 0.5.

--- src/visualize_band_diagram.py
+++ src/visualize_band_diagram.py
@@ -8,13 +8,13 @@ import math, cmath
 class t1DProblem:
     def __init__(self):
         self.Lattice = pc.tBravaisLattice([num.array([1])])
-        self.Mesh = fempy.mesh.tOneDimensionalMesh(-1, 1, 10, "floquet_l", "floquet_r")
+        self.Mesh = fempy.mesh.tOneDimensionalMesh(-1, 1, 100, "floquet_l", "floquet_r")
         self.LeftNode = fempy.solver.getNodesWithTrackingId(self.Mesh, "floquet_l")[0]
         self.RightNode = fempy.solver.getNodesWithTrackingId(self.Mesh, "floquet_r")[0]
 
         def eps(x):
-            if 0.4 <= x < 0.6:
-                return 4
+            if 0.4 <= x[0] < 0.6:
+                return 10
             else:
                 return 1
 
@@ -24,17 +24,19 @@ class t1DProblem:
 
     def kTrack(self):
         rl = self.Lattice.ReciprocalLattice
-        return [0*rl[0], rl[0]]
+        return [0.01*rl[0], 0.99*rl[0]]
 
     def getEigenvalues(self, k):
         print "computing for k = ",k
         self.EigenSolver.setupConstraints(
             {self.RightNode: (0, [(cmath.exp(1j*k[0]), self.LeftNode)])})
         pairs = self.EigenSolver.solve(0,
-                                       tolerance = 1e-10,
-                                       number_of_eigenvalues = 2)
+                                       tolerance = 1e-6,
+                                       number_of_eigenvalues = 7)
         
-        return [evalue for evalue, em in pairs]
+        result = [evalue for evalue, em in pairs]
+        result.sort(lambda e1, e2: cmp(abs(e1), abs(e2)))
+        return result
     
 class t2DProblem:
     def __init__(self):
@@ -51,13 +53,12 @@ class t2DProblem:
         pass
 
 problem = t1DProblem()
-k_track = tools.interpolateVectorList(problem.kTrack(), 30)
+k_track = tools.interpolateVectorList(problem.kTrack(), 20)
 
 def scale_eigenvalue(ev):
-    return abs(ev)
-    #return math.sqrt(abs(ev)) / (2 * math.pi)
+    return math.sqrt(abs(ev)) / (2 * math.pi)
 
-bdfile = file(",,band_diagram_analytic.data", "w")
+bdfile = file(",,band_diagram.data", "w")
 evlists = [problem.getEigenvalues(k) for k in k_track]
 for i in range(len(evlists[0])):
     for j in range(len(k_track)):
